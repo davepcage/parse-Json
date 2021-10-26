27588d2874e9db55e87ef3f03e4985d990bb3e46
davepcage <71347825+davepcage@users.noreply.github.com>
2021-10-26 12:16:02 -0500

    black Keys

--- Json.js
+++ Json.js
@@ -1,14 +1,17 @@
 document.getElementById("convert").addEventListener("click", function(){
   document.getElementById("count").innerHTML = " "
   document.getElementById("result").innerHTML = " "
-
-  cadenaFinal = document.getElementById("start").value.split(",").length
-  document.getElementById("count").innerHTML +=  "object[" + cadenaFinal + "]";
-  caracteres = document.getElementById("start").length
-  const cadena1 = document.getElementById("start").value.slice(1,caracteres).split(",")
+  cadenaFinal= ""
+  cadena = document.getElementById("start").value
+  contador = cadena.split(",").length
+  document.getElementById("count").innerHTML +=  "object[" + contador + "]";
+  caracteres = cadena.length
+  const cadena1 = cadena.slice(1,caracteres-1).split(",")
   console.log(cadena1)
   for (var i = 0 ; i <= cadena1.length-1; i++) {
-    cadena = cadena1[i].split(":")
-    document.getElementById("result").innerHTML += "<code style='color: blue;'>" + cadena[0] +" </code> : <code>" + cadena[1] +" </code><br>";
+    resultado = cadena1[i].split(":")
+    cadenaFinal += ("<code style='color: blue;'>" + resultado[0] +" </code> : <code>" + resultado[1] +" </code><br>");
   }
+  document.getElementById("result").innerHTML = "{ <br>" + cadenaFinal + "}"
+
 });
