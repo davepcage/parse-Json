2b62e8361147df2a66ab2063d82bbd4f9faad21e
davepcage <71347825+davepcage@users.noreply.github.com>
2021-10-27 17:23:51 -0500

    JSON.parse and Object.entries()

--- Json.js
+++ Json.js
@@ -2,16 +2,12 @@ document.getElementById("convert").addEventListener("click", function(){
   document.getElementById("count").innerHTML = " "
   document.getElementById("result").innerHTML = " "
   cadenaFinal= ""
-  cadena = document.getElementById("start").value
-  contador = cadena.split(",").length
-  document.getElementById("count").innerHTML +=  "object[" + contador + "]";
-  caracteres = cadena.length
-  const cadena1 = cadena.slice(1,caracteres-1).split(",")
-  console.log(cadena1)
-  for (var i = 0 ; i <= cadena1.length-1; i++) {
-    resultado = cadena1[i].split(":")
-    cadenaFinal += ("<code style='color: blue;'>" + resultado[0] +" </code> : <code>" + resultado[1] +" </code><br>");
+  cadena = Object.entries(JSON.parse(document.getElementById("start").value))
+  contador = cadena.length
+  document.getElementById("count").innerHTML +=  "object[" + contador + "]<br>";
+  for (var i = 0; i < contador; i++) {
+    c = cadena[i]
+       cadenaFinal += "<code style='color: blue;'>" + c[0]+" </code>:  <code>" +  c[1] +" </code><br>";
   }
-  document.getElementById("result").innerHTML = "{ <br>" + cadenaFinal + "}"
-
+    document.getElementById("result").innerHTML = "{ <br>" + cadenaFinal + "}"
 });
