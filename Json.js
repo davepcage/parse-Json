c671b9f0990afafc692b6e2f625073d17ae7a4c6
davepcage <71347825+davepcage@users.noreply.github.com>
2021-10-27 19:10:18 -0500

    test typeof

--- Json.js
+++ Json.js
@@ -2,12 +2,20 @@ document.getElementById("convert").addEventListener("click", function(){
   document.getElementById("count").innerHTML = " "
   document.getElementById("result").innerHTML = " "
   cadenaFinal= ""
-  cadena = Object.entries(JSON.parse(document.getElementById("start").value))
-  contador = cadena.length
+  ConvertToArray = Object.entries(JSON.parse(document.getElementById("start").value))
+  contador = ConvertToArray.length
   document.getElementById("count").innerHTML +=  "object[" + contador + "]<br>";
   for (var i = 0; i < contador; i++) {
-    c = cadena[i]
-       cadenaFinal += "<code style='color: blue;'>" + c[0]+" </code>:  <code>" +  c[1] +" </code><br>";
+    c = ConvertToArray[i]
+    if (typeof(c[1]) == String) {
+      c1 = "<code style = 'color:red'>" + c[1] + "</code>"
+    }else if(typeof(c[1]) == Number){
+       c1 = "<code style = 'color:orange'>" + c[1] + "</code>"
+    }else{
+       c1 = "<code style = 'color:yellow'>" + c[1] + "</code>"
+    }
+       cadenaFinal += "<code style='color: blue;'>" + c[0]+" </code>: " +  c1 +" <br>";
+  
   }
     document.getElementById("result").innerHTML = "{ <br>" + cadenaFinal + "}"
 });
