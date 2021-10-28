3127a563ec6d5048fbd37dd4798c6ea582da4160
davepcage <71347825+davepcage@users.noreply.github.com>
2021-10-28 10:18:16 -0500

    add colors to data type

--- Json.js
+++ Json.js
@@ -2,18 +2,27 @@ document.getElementById("convert").addEventListener("click", function(){
   document.getElementById("count").innerHTML = " "
   document.getElementById("result").innerHTML = " "
   cadenaFinal= ""
-  ConvertToArray = Object.entries(JSON.parse(document.getElementById("start").value))
-  contador = ConvertToArray.length
+  convertToArray = Object.entries(JSON.parse(document.getElementById("start").value))
+  contador = convertToArray.length
   document.getElementById("count").innerHTML +=  "object[" + contador + "]<br>";
   for (var i = 0; i < contador; i++) {
-    c = ConvertToArray[i]
-    if (typeof(c[1]) == String) {
+    c = convertToArray[i]
+    
+    if (typeof(c[1]) == "string") {
       c1 = "<code style = 'color:red'>" + c[1] + "</code>"
-    }else if(typeof(c[1]) == Number){
+    }else if(typeof(c[1]) == "number"){
        c1 = "<code style = 'color:orange'>" + c[1] + "</code>"
     }else{
-       c1 = "<code style = 'color:yellow'>" + c[1] + "</code>"
+      if (c[1] != null) {
+        list = c[1]
+        console.log(list)
+        c1 = "<code style = 'color:green'>" + c[1] + "</code>"
+
+      }else{
+       c1 = "<code style = 'color:pink'>" + c[1] + "</code>"
+      }
     }
+
        cadenaFinal += "<code style='color: blue;'>" + c[0]+" </code>: " +  c1 +" <br>";
   
   }
