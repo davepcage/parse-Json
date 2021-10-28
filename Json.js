935f8f7ea0e2dd1cc021650281e166a664812e45
davepcage <71347825+davepcage@users.noreply.github.com>
2021-10-28 14:53:57 -0500

    add format colors

--- Json.js
+++ Json.js
@@ -6,25 +6,31 @@ document.getElementById("convert").addEventListener("click", function(){
   contador = convertToArray.length
   document.getElementById("count").innerHTML +=  "object[" + contador + "]<br>";
   for (var i = 0; i < contador; i++) {
-    c = convertToArray[i]
+    array = convertToArray[i]
     
-    if (typeof(c[1]) == "string") {
-      c1 = "<code style = 'color:red'>" + c[1] + "</code>"
-    }else if(typeof(c[1]) == "number"){
-       c1 = "<code style = 'color:orange'>" + c[1] + "</code>"
+    if (typeof(array[1]) == "string") {
+      formatColor = "<code style = 'color:red'>" + array[1] + "</code>"
+    }else if(typeof(array[1]) == "number"){
+      formatColor = "<code style = 'color:orange'>" + array[1] + "</code>"
+    }else if (array[1] == null) {
+      formatColor = "<code style = 'color:pink'>" + array[1] + "</code>"
     }else{
-      if (c[1] != null) {
-        list = c[1]
-        console.log(list)
-        c1 = "<code style = 'color:green'>" + c[1] + "</code>"
-
-      }else{
-       c1 = "<code style = 'color:pink'>" + c[1] + "</code>"
+      subColor = ""
+      for (var e = 0;e < array[1].length; e++){
+        array[1][e]
+        if (e == array[1].length-1) {
+          subColor += "<code style = 'color:green'>" + "<br>"+ array[1][e] + "</code>"
+        }else{
+          subColor += "<code style = 'color:green'>" + "<br>"+ array[1][e] + ",</code>" 
+        }
       }
+      formatColor = "["+ subColor + "<br>]"
     }
-
-       cadenaFinal += "<code style='color: blue;'>" + c[0]+" </code>: " +  c1 +" <br>";
-  
+    if (i == contador-1) {
+      cadenaFinal += "<code style='color: blue;'>" + array[0]+" </code>: " +  formatColor +" <br>";
+    }else{
+      cadenaFinal += "<code style='color: blue;'>" + array[0]+" </code>: " +  formatColor +", <br>";
+    }
   }
-    document.getElementById("result").innerHTML = "{ <br>" + cadenaFinal + "}"
+  document.getElementById("result").innerHTML = "{ <br>" + cadenaFinal + "}"
 });
