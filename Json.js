0f028e9db29ee4791cad49e3d4a640c99bf78b70
davepcage <71347825+davepcage@users.noreply.github.com>
2021-11-04 09:39:31 -0600

    colors order

--- Json.js
+++ Json.js
@@ -1,11 +1,16 @@
-document.getElementById("convert").addEventListener("click", function(){
-  document.getElementById("count").innerHTML = " "
-  document.getElementById("result").innerHTML = " "
-  cadenaFinal= ""
-  convertToArray = Object.entries(JSON.parse(document.getElementById("start").value))
+var array = ""
+cadenaFinal = ""
+contador = 0
+document.getElementById("convert").addEventListener("click", function (){
+  document.getElementById("count").innerHTML = " ";
+  document.getElementById("result").innerHTML = " ";
+  valores = document.getElementById("start").value
+  convertToArray = Object.entries(JSON.parse(valores.split(",")))
   contador = convertToArray.length
-  document.getElementById("count").innerHTML +=  "object[" + contador + "]<br>";
-  
+  printItems(convertToArray);
+
+});
+function printItems() {
   for (var i = 0; i < contador; i++) {
     array = convertToArray[i]
     if (typeof(array[1]) == "string") {
@@ -13,32 +18,64 @@ document.getElementById("convert").addEventListener("click", function(){
     }else if(typeof(array[1]) == "number"){
       formatColor = "<code style = 'color:green'>" + array[1] + "</code>"
     }else if (array[1] == null) {
-      formatColor = "<code style = 'color:pink'>" + array[1] + "</code>"
-    }else{
-      subColor = ""
-      for (var e = 0;e < array[1].length; e++){
-        array[1][e]
-        if (typeof(array[1][e]) == "string") {
-          array[1][e] = "<code style = 'color:red'>" + array[1][e] + "</code>"
-        }else if (typeof(array[1][e]) == "number") {
-          array[1][e] = "<code style = 'color:green'>" + array[1][e] + "</code>"
-        }else {
-          array[1][e] = "<code style = 'color:pink'>" + array[1][e] + "</code>"
-        }
-        if (e == array[1].length-1) {
-          subColor += "<br>"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp" +  array[1][e];
-        }else{
-          subColor += "<br>"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp" +  array[1][e] + "," 
-        }
-      }
-      formatColor = "["+ subColor + "<br>]"
-    }
+      console.log(typeof(array[1]))
+      formatColor = "<code style = 'color:orange'>" + array[1] + "</code>"
+    }else if (typeof(array[1])) {
+      subarray = array[1]
+      if (Array.isArray(subarray)) {
+        subColor = ""
+        for (var e = 0;e < array[1].length; e++){
+          array[1][e]
+          if (typeof(array[1][e]) == "string") {
+            array[1][e] = "<code style = 'color:red'>" + array[1][e] + "</code>"
+          }else if (typeof(array[1][e]) == "number") {
+            array[1][e] = "<code style = 'color:green'>" + array[1][e] + "</code>"
+          }else {
+            array[1][e] = "<code style = 'color:pink'>" + array[1][e] + "</code>"
+          }
 
+          if (e == array[1].length-1) {
+            subColor += "<br>"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp" + array[1][e];
+          }else{
+            subColor += "<br>"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp" +  array[1][e] + "," 
+          }
+        }
+        formatColor = "["+ subColor + "<br>]"
+      }else{
+        subCadena= ""
+        subarray = Object.entries(subarray)
+        for (var j = 0; j < subarray.length; j++) {
+          subarray[j]
+          if (typeof(subarray[j][1]) == "string") {
+            arrayColors = "<code style = 'color:red'>" + subarray[j][1] + "</code>"
+          }else if(typeof(subarray[j][1]) == "number") {
+            arrayColors = "<code style = 'color:green'>" + subarray[j][1] + "</code>"
+            
+          }else if(typeof(subarray[j][1]) == null){
+            arrayColors = "<code style = 'color:pink'>" + subarray[j][1] + "</code>"
+          }
+
+          if (j == subarray.length-1) {
+            subCadena +="&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"<code style='color: blue;'>" + subarray[j][0]+ " </code>: " + arrayColors ;
+          }else{
+            subCadena += "<br>"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"<code style='color: blue;'>" + subarray[j][0]+ " </code>: " + arrayColors + ",<br>" 
+          }
+        }
+        cadenaFinal += "&nbsp"+"&nbsp"+"&nbsp"+"<code style='color: blue;'>" + array[0]+" </code>: { "+ subCadena +"<br>}";
+
+      }      
+    }
     if (i == contador-1) {
-      cadenaFinal += "&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"<code style='color: blue;'>" + array[0]+" </code>: " +  formatColor +" <br>";
+      cadenaFinal += "&nbsp"+"&nbsp"+"&nbsp"+"<code style='color: blue;'>" + array[0]+" </code>: " +  formatColor;
     }else{
-      cadenaFinal += "&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"<code style='color: blue;'>" + array[0]+" </code>: " +  formatColor +", <br>";
+      cadenaFinal += "&nbsp"+"&nbsp"+"&nbsp"+"<code style='color: blue;'>" + array[0]+" </code>: " +  formatColor +", <br>";
     }
   }
-  document.getElementById("result").innerHTML = "{ <br>" + cadenaFinal +"<br>}"
-});
+    document.getElementById("result").innerHTML = "{ <br>" + cadenaFinal +"<br>}"
+  };
+
+
+
+
+
+
