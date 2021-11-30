521a3877ec3fbb168a2a5c070dc3e255872342c5
davepcage <71347825+davepcage@users.noreply.github.com>
2021-11-29 22:00:00 -0600

    update_finish

--- Json.js
+++ Json.js
@@ -1,41 +1,70 @@
 document.getElementById("start").addEventListener("input", function (){
-  values = document.getElementById("start").value
-  result = printItems((JSON.parse(values)))
-  document.getElementById("result").innerHTML = result;
+  var values = document.getElementById("start").value;
+  var spaceLenght = 0;
+  var result =  printItems((JSON.parse(values)),spaceLenght);
+  document.getElementById("result").innerHTML = "";
+  document.getElementById("result").innerHTML =  result;
 });
 
-function printItems(valores){
-  result = ""
+function iterarSpace(spaceLenght) {
+  var spacer = ""
+  var spaceCharacter = "&nbsp;&nbsp;&nbsp;&nbsp;";
+  for (var i =0 ; i < spaceLenght; i++) {
+    spacer = spacer + spaceCharacter;
+  }
+  return spacer;
+};
+
+function comaBr(countComaBr,i){
+  var comaCharacter = ""
+  if ( i < countComaBr-1) {
+     return comaCharacter = ",<br>";
+  }else{
+    return comaCharacter = "<br>";
+  };
+};
+
+function printItems(valores,spaceLenght){
+  spaceLenght++
+  var result = "";
+
   if (typeof(valores) == "string") {
-    return "<code style = 'color:red'>" + valores + "</code>"
-    // console.log(valores)
+    return "<code style = 'color:red'>'" + valores + "'</code>";
 
   }else if (typeof(valores) == "number") {
-    return "<code style = 'color:green'>" + valores + "</code>"
-    // console.log(valores)
+    return "<code style = 'color:green'>" + valores + "</code>";
 
   }else if (valores == null) {
-    return "<code style = 'color:pink'>" + valores + "</code>"
-    // console.log(valores)
+    return "<code style = 'color:pink'>" + valores + "</code>";
 
-  }else if(Array.isArray(valores)){
+  }else if (typeof(valores) == "boolean") {
+    return  "<code style = 'color:orange'>" + valores + "</code>";
+  }
+  else if(Array.isArray(valores)){
     for (var i = 0; i <  valores.length; i++) {
-      valores[i]
-      result += "<br>"+ "&nbsp&nbsp&nbsp" + printItems(valores[i]);
+      var space = iterarSpace(spaceLenght);
+      var formatItems = printItems(valores[i],spaceLenght);
+      var coma = comaBr(valores.length,i);
+      result +=  space + formatItems + coma;
     }
-    return "[ " +  result + "<br>"
+    spaceLenght--
+    space = iterarSpace(spaceLenght);
+    return "[ <br>" +  result  + space +"]"
 
   }else if (typeof(valores) == "object") {
-    var entries = Object.entries(valores)
+    var entries = Object.entries(valores);
     for (var i =0; i < entries.length; i++ ) {
-      entries[i]
-
-      result += "<code style='color: blue;'> " + entries[i][0] + " </code> : " + printItems(entries[i][1]) + "<br>"
-
+      var space = iterarSpace(spaceLenght);
+      formatItems = printItems(entries[i][1],spaceLenght);
+      var coma = comaBr(entries.length,i);
+      result +=  space + "<code style='color: blue;'>"+ entries[i][0] + "</code>: " + formatItems + coma;
     }
-    return "{  <br>" + result +"}"
+    spaceLenght--
+    space = iterarSpace(spaceLenght)
+    
+    return  "{ <br>"  +result + space +"}"  
     
   }else{
-    return  "<code style = 'color:black'>++" + valores + "</code>"
-  }
-}
+    return  "<code style = 'color:black'>" + valores + "</code>"
+  };
+};
